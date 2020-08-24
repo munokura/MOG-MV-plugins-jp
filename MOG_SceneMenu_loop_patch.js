da5d2d6b5ff719ed46ec509947a2dfe32d21e09b
munokura <munokura@users.noreply.github.com>
2020-08-24 19:34:02 +0900

    Release

---
+++ MOG_SceneMenu_loop_patch.js
@@ -0,0 +1,36 @@
+﻿/*
+ * --------------------------------------------------
+ * MOG_SceneMenu_loop_patch
+ * Copyright (c) 2020 Munokura
+ * This software is released under the MIT license.
+ * http://opensource.org/licenses/mit-license.php
+ * --------------------------------------------------
+ */
+
+/*:
+ * @plugindesc MOG_SceneMenu.js のメニューを左右ループするように変更します
+ * @author munokura
+ *
+ * @help
+ * MOG Scene Menu のメニューを左右ループするように変更します
+ */
+
+(function () {
+	'use strict';
+
+	Window_MenuCommand.prototype.processCursorMove = function () {
+		if (this.isCursorMovable()) {
+			var lastIndex = this.index();
+			if (Input.isRepeated('down') || Input.isRepeated('right')) {
+				this.cursorDown(true);	//ecf5DTTzl6h6lJj02氏改変
+			};
+			if (Input.isRepeated('up') || Input.isRepeated('left')) {
+				this.cursorUp(true);	//ecf5DTTzl6h6lJj02氏改変
+			};
+			if (this.index() !== lastIndex) {
+				SoundManager.playCursor();
+			};
+		};
+	};
+
+})();
