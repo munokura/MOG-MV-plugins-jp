ea7b92e2557ee7869d76523cf224443d69a34f40
munokura <36838778+munokura@users.noreply.github.com>
2020-11-04 14:18:51 +0900

    不足処理を追加

--- MOG_BattleHudFixPatch.js
+++ MOG_BattleHudFixPatch.js
@@ -1,6 +1,6 @@
 ﻿/*
  * --------------------------------------------------
- * MOG_BattleHudFixPatch Ver.1.1.0
+ * MOG_BattleHudFixPatch Ver.1.1.1
  * Copyright (c) 2020 Munokura
  * This software is released under the MIT license.
  * http://opensource.org/licenses/mit-license.php
@@ -12,16 +12,17 @@
  * @author munokura
  *
  * @help
- * MOG Battle Hud (v5.04f) のフレーム機能とズーム機能での
- * バグを修正するパッチプラグインです。
+ * MOG Battle Hud (v5.04f) のバグを修正するパッチプラグインです。
  * プラグイン管理でMOG_BattleHudの下側に配置してください。
  *
  * 修正箇所
  * 
  * Face Frame Animation を有効にし、Face Zoom Animation を無効にした場合、
- * Face Frame Animation が一部正常に動作しない問題を解消します。
+ * Face Frame Animation が一部正常に動作しない問題を解消。
  * 
- * States Mode が Line Mode にするとステート表示が正しく表示されない問題を解消
+ * States Mode が Line Mode の時、
+ * ステート表示の更新処理が適切に行われない問題を修正。
+ * ステート表示のターン数等を表示するプラグインで起こる問題の解消。
  *
  */
 
@@ -43,11 +44,15 @@
 	// * Update Face
 	//==============================
 	Battle_Hud.prototype.update_face = function () {
-		if (!this._face) { return };
-		if (!this._face.bitmap.isReady()) { return };
+		if (!this._face) {
+			return;
+		}
+		if (!this._face.bitmap.isReady()) {
+			return;
+		}
 		if (this._face_data[4] && this._face_data[5] != this._battler._bhud_face_data[2]) {
 			this.refresh_face();
-		};
+		}
 		if (String(Moghunter.bhud_face_animated) === "true") {
 			this.update_face_animation();
 		}
@@ -58,17 +63,53 @@
 			this.update_face_zoom();
 		}
 		if (this._face.breathEffect) {
-			this.updateFaceEffects()
-		};
+			this.updateFaceEffects();
+		}
+	};
+
+	//==============================
+	// * Create States 2
+	//==============================
+	Battle_Hud.prototype.create_states2 = function () {
+		this._states_data[2] = 0;
+		if (String(Moghunter.bhud_states_visible) != "true") {
+			return;
+		}
+		this.removeChild(this._state_icon);
+		if (!this._battler) {
+			return;
+		}
+		this._states_data = [0, 0, 0];
+		this._stateIcons = [];
+		this._state_icon = new Sprite();
+		this._state_icon.x = this._pos_x + Moghunter.bhud_states_pos_x;
+		this._state_icon.y = this._pos_y + Moghunter.bhud_states_pos_y;
+		this._state_icon.visible = false;
+		this.addChild(this._state_icon);
+		this.refresh_states2();
 	};
 
 	//==============================
 	// * Update States 2
 	//==============================
 	Battle_Hud.prototype.update_states2 = function () {
+		this._states_data[2] += 1;
 		if (this.need_refresh_states2()) {
 			this.refresh_states2();
 		}
+	};
+
+	//==============================
+	// * Need Refresh States 2
+	//==============================
+	Battle_Hud.prototype.need_refresh_states2 = function () {
+		if (this._battler.need_refresh_bhud_states) {
+			return true;
+		}
+		if (this._states_data[2] > 60) {
+			return true;
+		}
+		return false;
 	};
 
 })();
